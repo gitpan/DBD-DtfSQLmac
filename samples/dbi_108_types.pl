210a8ad11328efacb1605bf8f861d7d53e706227
Thomas Wegner <toms_email@gmx.de>
2001-03-15 16:32:43 +0000

    Import of TWEGNER/DBD-DtfSQLmac-0.1201 from CPAN.
    gitpan-cpan-distribution: DBD-DtfSQLmac
    gitpan-cpan-version:      0.1201
    gitpan-cpan-path:         TWEGNER/DBD-DtfSQLmac-0.1201.tar.gz
    gitpan-cpan-author:       TWEGNER
    gitpan-cpan-maturity:     released

---
+++ samples/dbi_108_types.pl
@@ -0,0 +1,18 @@
+#! perl -w
+
+use DBI 1.08;
+
+@typearray = (
+
+qw/ SQL_ALL_TYPES
+	SQL_CHAR SQL_NUMERIC SQL_DECIMAL SQL_INTEGER SQL_SMALLINT
+	SQL_FLOAT SQL_REAL SQL_DOUBLE SQL_VARCHAR
+	SQL_DATE SQL_TIME SQL_TIMESTAMP
+	SQL_LONGVARCHAR SQL_BINARY SQL_VARBINARY SQL_LONGVARBINARY
+	SQL_BIGINT SQL_TINYINT
+/ );
+
+foreach $type (@typearray) {
+	$call = 'DBI::' . $type;
+	print $type, " = ", &$call ,"\n";
+}
